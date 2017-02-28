0f5dadce3e379f80cbb4cb2f6d18657ffa6ca77a
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-02-28 11:12:14 +0900

    document

---
+++ README.md
@@ -0,0 +1,13 @@
+![https://docs.rs/cargo-pack/badge.svg](https://docs.rs/cargo-pack/badge.svg)
+
+# `cargo-pack`
+
+An infrastructure library for `cargo-pack`ers.
+
+`cargo-pack`ers help you to package your applications into various formats to deploy.
+
+| name                     | description              |
+|--------------------------|--------------------------|
+| [`cargo-packa-docker`][] | pack into a docker image |
+
+[`cargo-packa-docker`]: https://github.com/KeenS/cargo-pack-docker
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! an infrastructure library for 'cargo-pack'ers.
+//! This crate provides only common features of `pack`ers, currently, files to package.
+//! Currently, you can write these metadata in Cargo.toml:
+//!
+//! ```toml
+//! [package.metadata.pack]
+//! # Not used for now. Reserved for future use
+//! default-packers = ["docker"]
+//! # files to pack in addition to binaries
+//! files = ["README.md"]
+//! ```
+
+
+#![deny(missing_docs)]
 extern crate cargo;
 extern crate rustc_serialize;
 extern crate toml as toml_crate;
@@ -15,30 +29,48 @@ use cargo::util::Config;
 use cargo::util::{toml, paths, errors};
 use rustc_serialize::Decodable;
 
+/// Errors and related
 pub mod error {
     error_chain!{
         foreign_links {
-            Io(::std::io::Error);
-            Cargo(Box<::cargo::CargoError>);
+            Io(::std::io::Error)
+                /// IO errors
+                ;
+            Cargo(Box<::cargo::CargoError>)
+                /// Erros from the `cargo` crate
+                ;
         }
     }
 }
 
 use error::*;
 
+
+/// Rust side of configurations in `Cargo.toml`
+///
+/// Cargo.toml will look like
+///
+/// ```toml
+/// [package.metadata.pack]
+/// default-packers = ["docker"]
+/// files = ["README.md"]
+/// ```
 #[derive(RustcDecodable, Debug)]
 pub struct PackConfig {
-    files: Vec<String>,
-    default_packers: Vec<String>,
+    /// files to pack into other than binaries
+    pub files: Vec<String>,
+    /// reserved for future usage.
+    pub default_packers: Vec<String>,
 }
 
+/// cargo-pack API
 pub struct CargoPack<'cfg> {
     ws: Workspace<'cfg>,
     package_name: Option<String>,
     pack_config: PackConfig,
 }
 
-pub fn lookup(v: Value, path: &str) -> Option<Value> {
+fn lookup(v: Value, path: &str) -> Option<Value> {
     let ref path = match toml_crate::Parser::new(path).lookup() {
         Some(path) => path,
         None => return None,
@@ -59,7 +91,7 @@ pub fn lookup(v: Value, path: &str) -> Option<Value> {
             }
             Value::Array(mut v) => {
                 match key.parse::<usize>().ok() {
-                    // FIXME: may change index
+                    // NOTICE: may change the index
                     Some(idx) if idx < v.len() => cur_value = v.remove(idx),
                     _ => return None,
                 }
@@ -74,6 +106,13 @@ pub fn lookup(v: Value, path: &str) -> Option<Value> {
 
 
 impl<'cfg> CargoPack<'cfg> {
+    /// create a new CargoPack value
+    ///
+    /// ```rust
+    /// let config = Config::default().unwrap();
+    /// let pack = CargoPack::new(&config, None);
+    /// ```
+
     pub fn new<'a, P: Into<Option<String>>>(config: &'cfg Config, package_name: P) -> Result<Self> {
         let package_name = package_name.into();
         let root = find_root_manifest_for_wd(None, config.cwd())?;
@@ -88,13 +127,18 @@ impl<'cfg> CargoPack<'cfg> {
             package_name: package_name,
         })
     }
+
+    /// returns the current working space of the package of `package_name`
     pub fn ws(&self) -> &Workspace<'cfg> {
         &self.ws
     }
+
+    /// returns the PackConfig value
     pub fn config(&self) -> &PackConfig {
         &self.pack_config
     }
 
+    /// returns the `Package` value of `package_name`
     pub fn package(&self) -> Result<&Package> {
         if let Some(ref name) = self.package_name {
             let packages = self.ws()
@@ -130,16 +174,19 @@ impl<'cfg> CargoPack<'cfg> {
         let root = toml::parse(&contents, &manifest, ws.config())?;
         let root = Value::Table(root);
         debug!("root: {:?}", root);
-        let pack_root = lookup(root, "package.metadata.pack").expect("");
+        let pack_root = lookup(root, "package.metadata.pack")
+            .expect("no package.metadate.pack found in Cargo.toml");
         let mut d = Decoder::new(pack_root);
         Ok(Decodable::decode(&mut d).map_err(|e| errors::human(e.to_string()))?)
     }
 
+    /// decode a value from the manifest toml file.
     pub fn decode_from_manifest<'a, T: Decodable>(&self) -> Result<T> {
         let package_name = self.package_name.as_ref().map(|s| s.as_ref());
         Self::decode_from_manifest_static(self.ws(), package_name)
     }
 
+    /// returns files defined in `package.metadata.pack.files` in the Cargo.toml.
     pub fn files(&self) -> &[String] {
         self.pack_config.files.as_ref()
     }
